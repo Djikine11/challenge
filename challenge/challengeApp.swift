742d46771ddc8a145339e0ee959cf44943db090c
Ousmane <djikine000@gmail.com>
2024-12-04 20:23:12 +0100

    Initial Commit

---
+++ challenge/challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengeApp.swift
+//  challenge
+//
+//  Created by Ousmane on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
